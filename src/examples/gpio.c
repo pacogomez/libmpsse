b9df381867c8310e3bcea7f69cf6728ebd750e37
Paco Gomez <pgomez@vmware.com>
2020-11-20 09:26:09 -0600

    build on macOS

--- src/examples/gpio.c
+++ src/examples/gpio.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpsse.h>
+#include <unistd.h>
 
 int main(void)
 {
@@ -8,7 +9,7 @@ int main(void)
 	int i = 0, retval = EXIT_FAILURE;
 
 	io = MPSSE(GPIO, 0, 0);
-	
+
 	if(io && io->open)
 	{
 		for(i=0; i<10; i++)
@@ -16,19 +17,19 @@ int main(void)
 			PinHigh(io, GPIOL0);
 			printf("GPIOL0 State: %d\n", PinState(io, GPIOL0, -1));
 			sleep(1);
-			
+
 			PinLow(io, GPIOL0);
 			printf("GPIOL0 State: %d\n", PinState(io, GPIOL0, -1));
 			sleep(1);
 		}
-	
+
 		retval = EXIT_SUCCESS;
 	}
 	else
 	{
 		printf("Failed to open MPSSE: %s\n", ErrorString(io));
 	}
-		
+
 	Close(io);
 
 	return retval;
--- src/fast.c
+++ src/fast.c
@@ -9,6 +9,11 @@
 #include <string.h>
 #include "mpsse.h"
 #include "support.h"
+#if LIBFTDI1 == 1
+  #include <libftdi1/ftdi.h>
+#else
+  #include <ftdi.h>
+#endif
 
 /* Builds a block buffer for the Fast* functions. For internal use only. */
 int fast_build_block_buffer(struct mpsse_context *mpsse, uint8_t cmd, unsigned char *data, int size, int *buf_size)
@@ -64,9 +69,9 @@ int FastWrite(struct mpsse_context *mpsse, char *data, int size)
 				{
 					txsize = mpsse->xsize;
 				}
-	
+
 				if(fast_build_block_buffer(mpsse, mpsse->tx, (unsigned char *) (data + n), txsize, &buf_size) == MPSSE_OK)
-				{	
+				{
 					if(raw_write(mpsse, fast_rw_buf, buf_size) == MPSSE_OK)
 					{
 						n += txsize;
@@ -90,7 +95,7 @@ int FastWrite(struct mpsse_context *mpsse, char *data, int size)
 			}
 		}
 	}
-		
+
 	return MPSSE_FAIL;
 }
 
@@ -144,11 +149,11 @@ int FastRead(struct mpsse_context *mpsse, char *data, int size)
 			}
 		}
 	}
-	
+
 	return MPSSE_FAIL;
 }
 
-/* 
+/*
  * Function to perform fast transfers in MPSSE.
  *
  * @mpsse - libmpsse context pointer.
@@ -203,5 +208,3 @@ int FastTransfer(struct mpsse_context *mpsse, char *wdata, char *rdata, int size
 
 	return MPSSE_FAIL;
 }
-
-
